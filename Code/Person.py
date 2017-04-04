48afe01ffc05fb341e38497b2b0f502999aad931
Jean Carlo <jeanCarloVegaBejarano@hotmail.com>
2017-04-03 20:59:34 -0600

    Delete Class Person

--- Code/Person.py
+++
@@ -1,8 +0,0 @@
-"""Clase Padre"""
-class Person:
-    def __init__(self, cedula, nombre, edad, email):
-
-        self.cedula = cedula  # La cédula va ser el ID de todas las personas
-        self.nombre = nombre
-        self.edad = edad
-        self.email = email
